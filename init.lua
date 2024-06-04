c204df445196e2bfeadb39112de44925b2d05e34
matteac <mateoeestn2@gmail.com>
2024-06-03 23:31:24 -0300

    mapping: lsp signature help mapping

--- init.lua
+++ init.lua
@@ -411,6 +411,7 @@ require('lazy').setup({
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
           map('lho', vim.lsp.buf.hover, 'Hover Documentation')
+          map('lsh', vim.lsp.buf.signature_help, 'Signature Help')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
