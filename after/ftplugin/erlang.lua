9fbec8af692f91a00c090421b353c560e375b51e
matteac <mateoeestn2@gmail.com>
2024-06-06 22:28:47 -0300

    erlang formatter

---
+++ after/ftplugin/erlang.lua
@@ -0,0 +1,15 @@
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  group = vim.api.nvim_create_augroup("rebar3fmt", { clear = true }),
+  pattern = "*.erl",
+  callback = function()
+    -- See https://github.com/AdRoll/rebar format
+    vim.fn.jobstart({ "rebar3", "format" }, {
+      on_stdout = function()
+        vim.cmd("e!") -- sync file
+      end
+    })
+  end
+})
--- init.lua
+++ init.lua
@@ -474,7 +474,8 @@ require('lazy').setup({
         gleam = {},
         rust_analyzer = {},
         zls = {},
-        gopls = {}
+        gopls = {},
+        elp = {}
       }
 
 
@@ -778,7 +779,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
@@ -809,6 +810,7 @@ require('lazy').setup({
     },
   },
 })
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
